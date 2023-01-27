3e22093e0ff182a11bfd7e4fca1464ab6e8ce6e4
Jonathan Vanegas <jonathanvanegasg@hotmail.com>
2023-01-26 21:52:03 -0500

    subiendo archivos

--- frontend/src/redux/actions/index.js
+++ frontend/src/redux/actions/index.js
@@ -13,7 +13,7 @@ import {
 
 // URL desde donde se hará el consumo de la API
 //const API_URL = "http://localhost:5000/api"; // local
-const API_URL = "https://todolist-tambo-flowers-ec0nk8mrr-jonathanvg97.vercel.app/api/";
+const API_URL = "https://todolist-tambo-flowers-ec0nk8mrr-jonathanvg97.vercel.app/api";
 
 // Función para agregar una nueva tarea
 //? Párametros: data -> Información de la tarea / dispatch -> Para disparar la acción desde el componente
